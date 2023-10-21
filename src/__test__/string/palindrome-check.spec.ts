55089c8314200f17e9e636f7a906029807c2d106
Umair Raza <umair.raza666@gmail.com>
2023-10-22 00:01:15 +0500

    chore: optimize palindrome check

--- src/__test__/string/palindrome-check.spec.ts
+++ src/__test__/string/palindrome-check.spec.ts
@@ -1,5 +1,9 @@
 import { describe, it, expect } from "vitest";
-import { palindromeCheck } from "../../string/palindrome-check";
+import {
+  palindromeCheck,
+  palindromeCheckPointer,
+  palindromeCheckRecursion,
+} from "../../string/palindrome-check";
 
 describe("Palindrome Check", () => {
   it("Checks the truthy value", () => {
@@ -7,5 +11,37 @@ describe("Palindrome Check", () => {
     expect(palindromeCheck("dad")).toBe(true);
     expect(palindromeCheck("no lemon, no melon")).toBe(true);
     expect(palindromeCheck("hannah")).toBe(true);
+  });
+
+  it("Checks falsy values", () => {
+    expect(palindromeCheck("boat")).toBe(false);
+  });
+});
+
+describe("Palindrome Check with Pointers", () => {
+  it("Checks the truthy value", () => {
+    expect(palindromeCheckPointer("racecar")).toBe(true);
+    expect(palindromeCheckPointer("dad")).toBe(true);
+    expect(palindromeCheckPointer("no lemon, no melon")).toBe(true);
+    expect(palindromeCheckPointer("hannah")).toBe(true);
+  });
+
+  it("Checks falsy values", () => {
+    expect(palindromeCheckPointer("boat")).toBe(false);
+    expect(palindromeCheckPointer("habnah")).toBe(false);
+  });
+});
+
+describe("Palindrome Check with Recursion", () => {
+  it("Checks the truthy value", () => {
+    expect(palindromeCheckRecursion("racecar")).toBe(true);
+    expect(palindromeCheckRecursion("dad")).toBe(true);
+    expect(palindromeCheckRecursion("no lemon, no melon")).toBe(true);
+    expect(palindromeCheckRecursion("hannah")).toBe(true);
+  });
+
+  it("Checks falsy values", () => {
+    expect(palindromeCheckRecursion("boat")).toBe(false);
+    expect(palindromeCheckRecursion("habnah")).toBe(false);
   });
 });
--- src/string/palindrome-check.ts
+++ src/string/palindrome-check.ts
@@ -1,3 +1,8 @@
+/**
+ * Time Complexity : O(n^2)
+ *
+ * Space Complexity: O(n)
+ */
 export function palindromeCheck(str: string): boolean {
   let newString = "";
 
@@ -11,4 +16,41 @@ export function palindromeCheck(str: string): boolean {
   }
 
   return newString === removedSpaces;
+}
+
+/*
+ * Optimized Palindrome
+ */
+
+/**
+ * Time Complexity : O(n)
+ *
+ * Space Complexity: O(1)
+ */
+export function palindromeCheckPointer(str: string): boolean {
+  let leftPointer = 0;
+  const cleanStr = str.replace(/[\s,]+/g, "");
+  let rightPointer = cleanStr.length - 1;
+
+  while (leftPointer < rightPointer) {
+    const check = cleanStr[leftPointer] === cleanStr[rightPointer];
+    if (check === false) return false;
+    leftPointer++;
+    rightPointer--;
+  }
+
+  return true;
+}
+
+/**
+ * Recursive Solution
+ *
+ */
+export function palindromeCheckRecursion(str: string, i: number = 0): boolean {
+  const cleanString = str.replace(/[\s,]+/g, "");
+  const j = cleanString.length - 1 - i;
+  if (i >= j) return true;
+  return (
+    cleanString[i] === cleanString[j] && palindromeCheckRecursion(str, i + 1)
+  );
 }
